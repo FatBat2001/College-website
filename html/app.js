d28b439a775ad72b5299a0e56e971f2d5defa3f4
Fatbat <adhamekhalede@gmail.com>
2022-05-03 18:21:57 +0200

    first commit

---
+++ html/app.js
@@ -0,0 +1,41 @@
+
+
+
+function myFunction1() {
+
+   document.getElementById("newCard1").style.display = "inline";
+   document.getElementById("newCard2").style.display = "none";
+   document.getElementById("newCard3").style.display = "none";
+   document.getElementById("newCard4").style.display = "none";
+   document.getElementById("newCard5").style.display = "none";
+ }
+ 
+function myFunction2() {
+   document.getElementById("newCard1").style.display = "none";
+   document.getElementById("newCard2").style.display = "inline";
+   document.getElementById("newCard3").style.display = "none";
+   document.getElementById("newCard4").style.display = "none";
+   document.getElementById("newCard5").style.display = "none";
+ }
+ function myFunction3() {
+    document.getElementById("newCard1").style.display = "none";
+    document.getElementById("newCard2").style.display = "none";
+    document.getElementById("newCard3").style.display = "inline";
+    document.getElementById("newCard4").style.display = "none";
+    document.getElementById("newCard5").style.display = "none";
+  }
+  function myFunction4() {
+     document.getElementById("newCard1").style.display = "none";
+     document.getElementById("newCard2").style.display = "none";
+     document.getElementById("newCard3").style.display = "none";
+     document.getElementById("newCard4").style.display = "inline";
+     document.getElementById("newCard5").style.display = "none";
+   }
+   function myFunction5() {
+      document.getElementById("newCard1").style.display = "none";
+      document.getElementById("newCard2").style.display = "none";
+      document.getElementById("newCard3").style.display = "none";
+      document.getElementById("newCard4").style.display = "none";
+      document.getElementById("newCard5").style.display = "inline";
+    }
+  
